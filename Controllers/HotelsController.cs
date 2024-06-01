a03b3ea34f20aac1e582d98752263b5cede1aae3
Ahmed Raza <kingminai3@gmail.com>
2024-06-02 00:04:01 +0200

    created the booking form

--- Controllers/HotelsController.cs
+++ Controllers/HotelsController.cs
@@ -40,6 +40,30 @@ namespace CapetropolisTourism.Controllers
             return NotFound();
         }
 
+        public IActionResult BookingSubmission(string name, string roomType)
+        {
+            Console.WriteLine("BookingSubmission: " + name + " " + roomType);
+            return RedirectToAction("Booking", new { name = name });
+            // HotelModel hotel = hotels.FirstOrDefault(h => h.name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            // if (hotel != null)
+            // {
+            //     RoomModel selectedRoom = hotel.rooms.FirstOrDefault(r => r.name.Equals(roomType, StringComparison.OrdinalIgnoreCase));
+
+            //     if (selectedRoom != null)
+            //     {
+            //         return View(new BookingModel
+            //         {
+            //             hotel = hotel,
+            //             room = selectedRoom,
+            //             meal = meal,
+            //             date = date
+            //         });
+            //     }
+            // }
+            // return NotFound();
+        }
+
         public JsonResult Details(string name)
         {
             HotelModel hotel = hotels.FirstOrDefault(h => h.name.Equals(name, StringComparison.OrdinalIgnoreCase));
@@ -49,6 +73,20 @@ namespace CapetropolisTourism.Controllers
                 return Json(hotel);
             }
             return Json(null);
+        }
+
+        public IActionResult Search(string query)
+        {
+            List<HotelModel> results = hotels.Where(h => h.name.Contains(query, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            return View(results);
+        }
+
+        public IActionResult Filter(string meal)
+        {
+            List<HotelModel> results = hotels.Where(h => h.meals.Contains(meal, StringComparer.OrdinalIgnoreCase)).ToList();
+
+            return View(results);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
